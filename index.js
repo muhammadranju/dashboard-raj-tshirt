a5e40023c6c6943bce14628cc620587937f6db29
Muhammad Ranju <mdranju23@gmail.com>
2024-09-28 00:32:44 +0600

    feat: Update project dependencies
    - Upgrade jsonwebtoken dependency
    - Upgrade string-strip-html dependency
    - Upgrade mongoose dependency
    - Upgrade body-parser dependency
    - Add @aws-sdk dependencies

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = require("./app/app");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 4050;
 server.listen(PORT, () => {
-    console.log(`Dashbord app is running on http://localhost:${PORT}`);
+  console.log(`Dashbord app is running on http://localhost:${PORT}`);
 });
